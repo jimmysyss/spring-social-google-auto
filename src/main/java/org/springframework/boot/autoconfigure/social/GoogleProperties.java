83065d07eb1ee3c7d750e52f089faac9c413d553
jimmy <jimmysyssie@hotmail.com>
2015-01-05 00:29:30 +0800

    Add New

---
+++ src/main/java/org/springframework/boot/autoconfigure/social/GoogleProperties.java
@@ -0,0 +1,8 @@
+package org.springframework.boot.autoconfigure.social;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("spring.social.google")
+public class GoogleProperties extends SocialProperties {
+
+}
